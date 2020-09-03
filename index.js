dfba1920f99bf4ada92f0c187cfc9910a00f6be0
greg <wegory@users.noreply.github.com>
2020-09-03 18:43:08 +0800

    Update and rename App.tsx to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import {
+  TourGuideProvider,
+  TourGuideZone,
+  TourGuideZoneByPosition,
+  useTourGuideController,
+} from './src'
+
+
+export {
+  TourGuideProvider,
+  TourGuideZone,
+  TourGuideZoneByPosition,
+  useTourGuideController,
+}
